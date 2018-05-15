23be1203c2febafb6196a625ebd549abc76175c6
mSimrat92 <32308915+mSimrat92@users.noreply.github.com>
2018-05-14 23:09:04 -0400

    Final commit

---
+++ app/src/main/java/com/example/sachinsharma/cricfun/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.sachinsharma.cricfun;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout mDrawerLayout;
+    ViewPager vp;
+    Fragment fragment = null;
+    private View navHeader;
+    NavigationView navigationView;
+    private ImageView imgNavHeaderBg, imgProfile;
+    TextView nametxt,idText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navHeader=navigationView.getHeaderView(0);
+        nametxt=navHeader.findViewById(R.id.name);
+        idText=navHeader.findViewById(R.id.email);
+        imgProfile=navHeader.findViewById(R.id.image_profile);
+        nametxt.setText("Cricket App");
+        idText.setText("info@cricketapp.com");
+       //imgProfile.setImageResource(R.drawable.download);
+        navigationView.setNavigationItemSelectedListener(this);
+        //=findViewById(R.id.drawer_layout);
+        vp=findViewById(R.id.vpPager);
+        vp.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),this));
+        vp.setCurrentItem(1,true);
+
+
+        //displayMatches();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+           Intent intent=new Intent(this,MainActivity.class);
+           startActivity(intent);
+        } else if (id == R.id.nav_currentMatches) {
+            findViewById(R.id.mainLinear).setVisibility(View.GONE);
+            fragment=new CurrentMatchesFragment().newInstance();
+        }
+        else if (id == R.id.nav_Results) {
+            findViewById(R.id.mainLinear).setVisibility(View.GONE);
+            fragment=new ResultsFragment().newInstance();
+        } else if (id == R.id.nav_News) {
+
+        } else if (id == R.id.nav_videos) {
+
+        } else if (id == R.id.nav_photos) {
+
+        }
+        else if(id == R.id.nav_pointsTable){
+
+        }
+        else if(id== R.id.nav_PlayerProfiles){
+           Intent intent=new Intent(this,TwitterActivity.class);
+           startActivity(intent);
+        }
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.mainConstraint, fragment);
+            Log.d("TAG", "Item Click Working");
+            ft.commit();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sachinsharma/cricfun/MatchesLive.java
@@ -0,0 +1,59 @@
+package com.example.sachinsharma.cricfun;
+
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by sachinsharma on 1/1/18.
+ */
+
+public class MatchesLive {
+
+    int dateofmatch;
+    List<MatchView> matches;
+
+    public MatchesLive(){
+        matches=MatchView.listAll(MatchView.class);
+        if(matches.size()==0) {
+            MatchView matchView1 = new MatchView("Ind \uD83C\uDDEE\uD83C\uDDF3", "\uD83C\uDDFF\uD83C\uDDE6 SA", "10:00", "18");
+            matchView1.save();
+            MatchView matchView2 = new MatchView("Aus \uD83C\uDDE6\uD83C\uDDFA", "\uD83C\uDDEC\uD83C\uDDE7 Eng", "10:00", "18");
+            matchView2.save();
+            MatchView matchView3 = new MatchView("NZ \uD83C\uDDF3\uD83C\uDDFF", "\uD83C\uDDF9\uD83C\uDDF9 WI", "10:00", "18");
+            matchView3.save();
+            MatchView matchView4 = new MatchView("Pak \uD83C\uDDF5\uD83C\uDDF0", "\uD83C\uDDEE\uD83C\uDDEA Ire", "10:00", "17");
+            matchView4.save();
+            MatchView matchView5 = new MatchView("Ind \uD83C\uDDEE\uD83C\uDDF3", "\uD83C\uDDFF\uD83C\uDDE6 SA", "10:00", "19");
+            matchView5.save();
+            MatchView matchView6 = new MatchView("NZ \uD83C\uDDF3\uD83C\uDDFF", "\uD83C\uDDEE\uD83C\uDDEA Ire", "10:00", "18");
+            matchView6.save();
+            MatchView matchView7 = new MatchView("NZ \uD83C\uDDF3\uD83C\uDDFF","\uD83C\uDDEE\uD83C\uDDF3 Ind" , "10:00", "19");
+            matchView7.save();
+            MatchView matchView8 = new MatchView("WI \uD83C\uDDF9\uD83C\uDDF9", "\uD83C\uDDF5\uD83C\uDDF0 Pak", "10:00", "17");
+            matchView8.save();
+            MatchView matchView9 = new MatchView("Ire \uD83C\uDDEE\uD83C\uDDEA", "\uD83C\uDDEE\uD83C\uDDF3 Ind", "10:00", "17");
+            matchView9.save();
+            MatchView matchView10 = new MatchView("Pak \uD83C\uDDF5\uD83C\uDDF0", "\uD83C\uDDEE\uD83C\uDDF3 Ind", "10:00", "19");
+            matchView10.save();
+        }
+    }
+
+
+    public List<MatchView> returnMatches(String date){
+        matches=MatchView.findWithQuery(MatchView.class,"select * from Match_View where date=?",date);
+        return matches;
+    }
+
+    public int getSize(){
+        matches=MatchView.listAll(MatchView.class);
+        return matches.size();
+    }
+
+
+    public String toString(){
+        return "MatchesLive{"+
+                "matchesLive="+matches+
+                '}';
+    }
+
+}
